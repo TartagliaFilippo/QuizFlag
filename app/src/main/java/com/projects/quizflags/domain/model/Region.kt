3af966afe9989b9aa2908b076f6b63cad93dedb3
TartagliaFilippo <pippotartaglia@gmail.com>
2025-09-05 20:43:32 +0200

    update region model

--- app/src/main/java/com/projects/quizflags/domain/model/Region.kt
+++ app/src/main/java/com/projects/quizflags/domain/model/Region.kt
@@ -1,9 +1,20 @@
 package com.projects.quizflags.domain.model
 
-data class Region(
-    val id: Int,
+import com.projects.quizflags.R
+
+enum class Region(
     val code: String,
-    val nameIta: String,
-    val nameEng: String,
-    val nameEsp: String,
-)
+    val nameRes: Int
+) {
+    EUROPE("eu", R.string.region_europe),
+    ASIA("as", R.string.region_asia),
+    AFRICA("af", R.string.region_africa),
+    NORTH_AMERICA("na", R.string.region_north_america),
+    SOUTH_AMERICA("sa", R.string.region_south_america),
+    OCEANIA("oc", R.string.region_oceania),
+    ANTARCTICA("an", R.string.region_antarctica);
+
+    companion object {
+        fun fromCode(code: String): Region? = values().find { it.code == code }
+    }
+}
